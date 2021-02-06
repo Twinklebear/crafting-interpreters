c23cd7ebfee556d5fac26b6a862691d77554f62e
Will Usher <will.usher@hey.com>
2021-02-05 22:44:00 -0700

    missing expect files

--- interpreter/tests/run_tests.sh
+++ interpreter/tests/run_tests.sh
@@ -2,7 +2,7 @@
 
 for f in `ls $TEST_DIR/*.js`; do
     script_name=`basename $f`
-    expect="$TEST_DIR/expect/$script_name.out"
+    expect="$TEST_DIR/expect/$script_name.expect"
     $TEST_DIR/run_test.py $f $expect
     if [[ "$?" != "0" ]]; then
         exit 1
