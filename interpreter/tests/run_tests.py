9cbe544a079d0f17eb74ae491d0b37a7fa2c026d
Will Usher <will.usher@hey.com>
2021-02-06 15:08:15 -0700

    run all tests from python instead of bash -> python

---
+++ interpreter/tests/run_tests.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import subprocess
+import glob
+
+ANSI_RED = "\033[91m"
+ANSI_GREEN = "\033[92m"
+ANSI_END = "\033[0m"
+
+test_dir = os.getenv("TEST_DIR")
+failed_tests = 0
+for test_input in glob.glob("{}/*.js".format(test_dir)):
+    script_name = os.path.basename(test_input)
+    expect_out_file = "{}/expect/{}.expect".format(test_dir, script_name)
+    print("Running test '{}':".format(script_name), end=" ")
+    with open(expect_out_file, "r") as expect_file:
+        expect_output = expect_file.read()
+        result = subprocess.run(["./interpreter", test_input], stdout=subprocess.PIPE)
+        output = result.stdout.decode("utf-8")
+        if expect_output != output:
+            failed_tests += 1
+            print(ANSI_RED + "Failed" + ANSI_END)
+        else:
+            print(ANSI_GREEN + "Passed" + ANSI_END)
+
+if failed_tests != 0:
+    print("Some tests did not pass")
+    sys.exit(1)
+else:
+    print("All tests passed")
+
