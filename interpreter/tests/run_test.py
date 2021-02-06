91ca370a5f9ea3c96e98b274638cc0ae57dd8137
Will Usher <will.usher@hey.com>
2021-02-05 22:47:10 -0700

    older python3 version on github ci

--- interpreter/tests/run_test.py
+++ interpreter/tests/run_test.py
@@ -6,7 +6,7 @@ import subprocess
 
 with open(sys.argv[2], "r") as f:
     expect_output = f.read()
-    result = subprocess.run(["./interpreter", sys.argv[1]], capture_output=True)
+    result = subprocess.run(["./interpreter", sys.argv[1]], stdout=subprocess.PIPE)
     output = result.stdout.decode("utf-8")
     if expect_output != output:
         sys.exit(1)
