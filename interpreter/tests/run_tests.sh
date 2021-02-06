33f208672b2fde7e84d089325b9a30d238a85f05
Will Usher <will.usher@hey.com>
2021-02-05 22:52:15 -0700

    run all tests evne if some fail

--- interpreter/tests/run_tests.sh
+++ interpreter/tests/run_tests.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+failed=0
 for f in `ls $TEST_DIR/*.js`; do
     script_name=`basename $f`
-    echo "Run test '$script_name'"
+    printf "Run test '$script_name': "
     expect="$TEST_DIR/expect/$script_name.expect"
     $TEST_DIR/run_test.py $f $expect
     if [[ "$?" != "0" ]]; then
         echo "Failed"
-        exit 1
+        failed=1
+    else
+        echo "Passed"
     fi
-    echo "Passed"
 done
+
+if [[ $failed -eq 1 ]]; then
+    echo "Some tests did not pass"
+    exit 1
+fi
+echo "All tests passed"
 
