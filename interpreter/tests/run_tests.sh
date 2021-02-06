ed2871c89c53726d2552b94dddc9a7c4b68f8649
Will Usher <will.usher@hey.com>
2021-02-05 22:50:11 -0700

    some log info in testing

--- interpreter/tests/run_tests.sh
+++ interpreter/tests/run_tests.sh
@@ -2,10 +2,13 @@
 
 for f in `ls $TEST_DIR/*.js`; do
     script_name=`basename $f`
+    echo "Run test '$script_name'"
     expect="$TEST_DIR/expect/$script_name.expect"
     $TEST_DIR/run_test.py $f $expect
     if [[ "$?" != "0" ]]; then
+        echo "Failed"
         exit 1
     fi
+    echo "Passed"
 done
 
